c9431aa1947119425b1d7d9d6fa079e8eb0b36f8
Kirk Webb <kwebb@cs.utah.edu>
2020-08-19 15:54:08 -0600

    Fix.

--- radio.py
+++ radio.py
@@ -14,6 +14,7 @@ class Radio:
         self.channel = chan
         self.rxstreamer = None
         self.txstreamer = None
+        self._setup_streamers()
 
     def _setup_streamers(self):
         # Set up streamers
