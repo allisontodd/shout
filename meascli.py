81053c3e8f62b391cce1519d3da1faeebd40da9c
Kirk Webb <kwebb@cs.utah.edu>
2020-08-29 20:51:15 -0600

    Tune.

--- meascli.py
+++ meascli.py
@@ -35,9 +35,9 @@ def get_avg_power(samps):
 
 class MeasurementsClient:
     XMIT_SAMPS_MIN = 100000
-    SEND_SAMPS_COUNT = 4
+    SEND_SAMPS_COUNT = 5
     FOFF = 1e4
-    TOFF = 0.2
+    TOFF = 0.5
     
     def __init__(self, servaddr, servport, radio_args = ""):
         self.pipe = None
