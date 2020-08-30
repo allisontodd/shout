9d1d1cf4028628d056f1c149f62f1b12098a7fc4
Kirk Webb <kwebb@cs.utah.edu>
2020-08-29 20:13:53 -0600

    Fix.

--- meascon.py
+++ meascon.py
@@ -148,7 +148,8 @@ class MeasurementsController:
 
     def cmd_measpaths(self, cmd):
         clients = self._get_client_list(cmd)
-        del cmd['client_list'] if 'client_list' in cmd
+        if 'client_list' in cmd:
+            del cmd['client_list']
         toff = cmd['toff'] if 'toff' in cmd else self.DEF_TOFF
         dfile = self._get_datafile()
         if not 'measure_paths' in dfile:
