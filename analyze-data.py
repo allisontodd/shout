ebc5f29905752a3c41d8e08d01c1efdaef6b4378
Kirk Webb <kwebb@cs.utah.edu>
2020-09-04 00:05:07 -0600

    More functionality in measurement diff output.

--- analyze-data.py
+++ analyze-data.py
@@ -14,6 +14,24 @@ DEF_DFNAME="measurements.hdf5"
 MEAS_ROOT="measure_paths"
 DEF_FILTBW = 1e5
 
+def get_powerdiffs(attrs, rxds, filtbw):
+    rate = attrs['rate']
+    fstep = attrs['freq_step']
+    steps = int(np.floor(rate/fstep/2))
+    nsamps = attrs['nsamps']
+    foff = filtbw/2
+    pwrs = []
+    for i in range(1,steps):
+        rsamps = rxds[0][(i-1)*nsamps:i*nsamps]
+        tsamps = rxds[1][(i-1)*nsamps:i*nsamps]
+        rsamps = butter_filt(rsamps, i*fstep - foff,
+                             i*fstep + foff, rate)
+        tsamps = butter_filt(tsamps, i*fstep - foff,
+                             i*fstep + foff, rate)
+        pwr = [get_avg_power(s) for s in (rsamps, tsamps)]
+        pwrs.append(pwr[1] - pwr[0])
+    return pwrs
+
 def main(args):
     dsfile = h5py.File("%s/%s" % (args.datadir, args.dfname), "r")
     if args.listds:
@@ -22,22 +40,20 @@ def main(args):
         run = dsfile[MEAS_ROOT][args.runstamp]
         rate = run.attrs['rate']
         if run.attrs['get_samples']:
+            if args.txname:
+                txgrp = run[args.txname]
+                if args.rxname:
+                    rxds = txgrp[args.rxname]
+                    pwrs = get_powerdiffs(run.attrs, rxds, args.filtbw)
+                    print(pwrs)
+                else:
+                    for rxname, rxds in txgrp.items():
+                        rxds = txgrp[args.rxname]
+                        pwrs = get_powerdiffs(run.attrs, rxds, args.filtbw)
+                        print(pwrs)
             for txname, txgrp in run.items():
                 for rxname, rxds in txgrp.items():
-                    fstep = run.attrs['freq_step']
-                    steps = int(np.floor(rate/fstep/2))
-                    nsamps = run.attrs['nsamps']
-                    pwrs = []
-                    foff = args.filtbw/2
-                    for i in range(1,steps):
-                        rsamps = rxds[0][(i-1)*nsamps:i*nsamps]
-                        tsamps = rxds[1][(i-1)*nsamps:i*nsamps]
-                        rsamps = butter_filt(rsamps, i*fstep - foff,
-                                             i*fstep + foff, rate)
-                        tsamps = butter_filt(tsamps, i*fstep - foff,
-                                             i*fstep + foff, rate)
-                        pwr = [get_avg_power(s) for s in (rsamps, tsamps)]
-                        pwrs.append(pwr[1] - pwr[0])
+                    pwrs = get_powerdiffs(run.attrs, rxds, args.filtbw)
                     print(pwrs)
         else:
             for txname, txgrp in run.items():
@@ -65,6 +81,8 @@ def parse_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("-o", "--datadir", type=str, default=DEF_OUTDIR)
     parser.add_argument("-f", "--dfname", type=str, default=DEF_DFNAME)
+    parser.add_argument("--txname", type=str)
+    parser.add_argument("--rxname", type=str)
     parser.add_argument("-l", "--listds", action="store_true")
     parser.add_argument("-m", "--measdiff", action="store_true")
     parser.add_argument("-p", "--plotpsd", action="store_true")
