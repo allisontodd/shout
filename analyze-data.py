398194ef61f4106f3d804147060029bcfa5b33ca
Kirk Webb <kwebb@cs.utah.edu>
2020-09-03 23:06:03 -0600

    Added plot function to analyzer

--- analyze-data.py
+++ analyze-data.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import argparse
+import multiprocessing as mp
 
 import numpy as np
 import scipy.signal as sig
@@ -18,32 +19,46 @@ def main(args):
     if args.listds:
         dsfile.visit(print)
     if args.measdiff:
-        if args.runstamp:
-            run = dsfile[MEAS_ROOT][args.runstamp]
-            rate = run.attrs['rate']
-            if run.attrs['get_samples']:
-                for txname, txgrp in run.items():
-                    for rxname, rxds in txgrp.items():
-                        fstep = run.attrs['freq_step']
-                        steps = int(np.floor(rate/fstep/2))
-                        nsamps = run.attrs['nsamps']
-                        pwrs = []
-                        foff = args.filtbw/2
-                        for i in range(1,steps):
-                            rsamps = rxds[0][(i-1)*nsamps:i*nsamps]
-                            tsamps = rxds[1][(i-1)*nsamps:i*nsamps]
-                            rsamps = butter_filt(rsamps, i*fstep - foff,
-                                                 i*fstep + foff, rate)
-                            tsamps = butter_filt(tsamps, i*fstep - foff,
-                                                 i*fstep + foff, rate)
-                            pwr = [get_avg_power(s) for s in (rsamps, tsamps)]
-                            pwrs.append(pwr[1] - pwr[0])
-                        print(pwrs)
-            else:
-                for txname, txgrp in run.items():
-                    for rxname, rxds in txgrp.items():
-                        print(rxds[1]-rxds[0])
+        run = dsfile[MEAS_ROOT][args.runstamp]
+        rate = run.attrs['rate']
+        if run.attrs['get_samples']:
+            for txname, txgrp in run.items():
+                for rxname, rxds in txgrp.items():
+                    fstep = run.attrs['freq_step']
+                    steps = int(np.floor(rate/fstep/2))
+                    nsamps = run.attrs['nsamps']
+                    pwrs = []
+                    foff = args.filtbw/2
+                    for i in range(1,steps):
+                        rsamps = rxds[0][(i-1)*nsamps:i*nsamps]
+                        tsamps = rxds[1][(i-1)*nsamps:i*nsamps]
+                        rsamps = butter_filt(rsamps, i*fstep - foff,
+                                             i*fstep + foff, rate)
+                        tsamps = butter_filt(tsamps, i*fstep - foff,
+                                             i*fstep + foff, rate)
+                        pwr = [get_avg_power(s) for s in (rsamps, tsamps)]
+                        pwrs.append(pwr[1] - pwr[0])
+                    print(pwrs)
+        else:
+            for txname, txgrp in run.items():
+                for rxname, rxds in txgrp.items():
+                    print(rxds[1]-rxds[0])
 
+    if args.plotpsd:
+        run = dsfile[MEAS_ROOT][args.runstamp]
+        rate = run.attrs['rate']
+        for txname, txgrp in run.items():
+            for rxname, rxds in txgrp.items():
+                fstep = run.attrs['freq_step']
+                steps = int(np.floor(rate/fstep/2))
+                nsamps = run.attrs['nsamps']
+                for i in range(1,steps):
+                    tsamps = rxds[1][(i-1)*nsamps:i*nsamps]
+                    psd = compute_psd(nsamps, tsamps)
+                    freqs = np.fft.fftshift(np.fft.fftfreq(nsamps, 1/rate))
+                    plproc = mp.Process(target=plot_stuff,
+                                        args=(rxname, freqs, psd))
+                    plproc.start()
 
 def parse_args():
     """Parse the command line arguments"""
@@ -52,6 +67,7 @@ def parse_args():
     parser.add_argument("-f", "--dfname", type=str, default=DEF_DFNAME)
     parser.add_argument("-l", "--listds", action="store_true")
     parser.add_argument("-m", "--measdiff", action="store_true")
+    parser.add_argument("-p", "--plotpsd", action="store_true")
     parser.add_argument("-t", "--runstamp", type=str, default=0)
     parser.add_argument("-b", "--filtbw", type=float, default=DEF_FILTBW)
     return parser.parse_args()
